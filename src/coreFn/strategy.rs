7e805c58d93b267f3b0249b785c1e9a12c3f1ee4
erran <3011509711@qq.com>
2026-03-10 14:10:32 +0800

    new1

--- src/coreFn/strategy.rs
+++ src/coreFn/strategy.rs
@@ -20,21 +20,21 @@ pub fn strategy(okx: &Quote, bybit: &Quote, ct_mult: f64, state: &State) -> Opti
     if okx.ts.abs_diff(bybit.ts) > 80 {
         return None;
     }
-    // let entry_spread = (bybit.bid_px - okx.ask_px) / okx.ask_px;
-    let entry_spread = (okx.bid_px - bybit.ask_px) / bybit.ask_px;
-    println!("{}",entry_spread);
+    let entry_spread = (bybit.bid_px - okx.ask_px) / okx.ask_px;
+    // let entry_spread = (okx.bid_px - bybit.ask_px) / bybit.ask_px;
+    // println!("{}",entry_spread);
     let exit_spread = (bybit.ask_px - okx.bid_px) / okx.bid_px;
-    // match *state {
-    //     State::Idle => {
-    //         if okx.bid_sz * ct_mult >= 10.0 && bybit.ask_sz >= 10.0 && entry_spread >= 0.006 {
-    //             return Some(Signal::OpenOkxLongBybitShort);
-    //         }
-    //     }
-    //     State::Opened => {
-    //         if exit_spread <= 0.0003 {
-    //             return Some(Signal::Close);
-    //         }
-    //     }
-    // }
+    match *state {
+        State::Idle => {
+            if okx.bid_sz * ct_mult >= 10.0 && bybit.ask_sz >= 10.0 && entry_spread >= 0.006 {
+                return Some(Signal::OpenOkxLongBybitShort);
+            }
+        }
+        State::Opened => {
+            if exit_spread <= 0.0003 {
+                return Some(Signal::Close);
+            }
+        }
+    }
     None
 }
